c9e0a65ff952470b961d464000feeee0e9108ac4
Kostas Tzounopoulos <konstantinos.tzounopoulos@internations.org>
2015-06-16 16:41:47 +0200

    Add shutdown functionality for the Lazy Kernel

--- src/InterNations/Component/Testing/SymfonyKernelTrait.php
+++ src/InterNations/Component/Testing/SymfonyKernelTrait.php
@@ -3,18 +3,28 @@ namespace InterNations\Component\Testing;
 
 trait SymfonyKernelTrait
 {
+    /** @var string */
+    private $kernelKey;
+
     public function bootLazyKernel(array $options = [])
     {
-        $key = '__SYMFONY_TEST_KERNEL';
+        $this->kernelKey = '__SYMFONY_TEST_KERNEL';
+
         if (!empty($options)) {
-            $key .= '_' . hash('sha256', (serialize($options)));
+            $this->kernelKey .= '_' . hash('sha256', (serialize($options)));
         }
 
-        if (!isset($GLOBALS[$key])) {
-            $GLOBALS[$key] = static::createKernel($options);
-            $GLOBALS[$key]->boot();
+        if (!isset($GLOBALS[$this->kernelKey])) {
+            $GLOBALS[$this->kernelKey] = static::createKernel($options);
+            $GLOBALS[$this->kernelKey]->boot();
         }
 
-        return $GLOBALS[$key];
+        return $GLOBALS[$this->kernelKey];
+    }
+
+    public function shutdownLazyKernel()
+    {
+        $GLOBALS[$this->kernelKey]->shutdown();
+        unset($GLOBALS[$this->kernelKey]);
     }
 }
